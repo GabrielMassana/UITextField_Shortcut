fe5d4ddc2aa315df2af4d6a403979207db28e723
Gabriel Massana <Gabi@LOCK8s-Mac-mini.local>
2014-04-07 10:29:08 +0100

    first commit

--- shortcut/ViewController.m
+++ shortcut/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) IBOutlet UITextField *textField;
+
 @end
 
 @implementation ViewController
@@ -17,7 +19,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(shortcut:) name:UITextFieldTextDidChangeNotification object:self.textField];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +28,64 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void) shortcut: (NSNotification*) notification
+{
+    UITextField *notificationTextField = [notification object];
+
+    if (notificationTextField == self.textField)
+    {
+        [self checkDoubleA:notificationTextField];
+        [self checkDoubleAB:notificationTextField];
+        [self checkDoubleSpace:notificationTextField];
+    }
+}
+
+-(void) checkDoubleA: (UITextField*) textField
+{
+    NSMutableString *string =  [textField.text mutableCopy];
+    NSRange range = [string rangeOfString:@"aa"];
+    if (range.location == NSNotFound)
+    {
+        NSLog(@"string was not found");
+    }
+    else
+    {
+        [string replaceCharactersInRange:range withString:@"ä"];
+    }
+    textField.text = string;
+}
+
+-(void) checkDoubleAB: (UITextField*) textField
+{
+    NSMutableString *string =  [textField.text mutableCopy];
+    NSRange range = [string rangeOfString:@"ab"];
+    if (range.location == NSNotFound)
+    {
+        NSLog(@"string was not found");
+    }
+    else
+    {
+        [string replaceCharactersInRange:range withString:@"XYZ"];
+    }
+    textField.text = string;
+}
+
+- (void) checkDoubleSpace: (UITextField*) textField
+{
+    NSMutableString *string =  [textField.text mutableCopy];
+    NSRange range = [string rangeOfString:@"  "];
+    if (range.location == NSNotFound)
+    {
+        NSLog(@"String was not found");
+    }
+    else
+    {
+        [string replaceCharactersInRange:range withString:@" "];
+    }
+    textField.text = string;
+}
+
+
 
 @end
